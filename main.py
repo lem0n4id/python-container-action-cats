efa7bd0862c6c4dcb518116078def9591816ce80
lem0n4id <61219881+lem0n4id@users.noreply.github.com>
2021-11-22 16:07:38 +0530

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-GITHUB_ACTION = os.environ["GITHUB_ACTION"]
+GITHUB_ACTION = os.environ["GITHUB_ACTOR"]
 
 if __name__ == "__main__":
-    print(GITHUB_ACTION)
+    print(F"{GITHUB_ACTION} initiated the workflow")
